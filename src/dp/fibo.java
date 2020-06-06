d0a409c5faf9ac61ba0a6107a29391a99d006e76
limca <ankit.rajput0404@gmail.com>
2020-06-06 23:48:23 +0530

    fibo corrected with more dp explaination

--- src/dp/fibo.java
+++ src/dp/fibo.java
@@ -3,6 +3,8 @@
  */
 package dp;
 
+import java.util.Arrays;
+
 /**
  * @author limca
  *
@@ -15,27 +17,43 @@ public class fibo {
 	public static void main(String[] args) {
 
 		int n = 6;
-		int[] memo = new int[n];
+		int[] memo = new int[n+1];
 		//memo[0]=1;
 		//memo[1]=1;
+		Arrays.fill(memo,-1);
 
 		System.out.println(fibo(n, memo));
+		
+		System.out.println(fibo(n, memo)==fibotopdown(n, memo));
 	}
 
+	//recursive approach
 	private static int fibo(int n,int memo[]) {
 	
-		if(n==0)
-			return 0;
-		if(n==1 || n==2) {
-			return 1;
+		if(n<=1) {
+			return n;
 			
 		}
+		if(memo[n]!=-1)
+			return memo[n];
 		else
 		{
-			memo[n-1]=fibo(n-1, memo)+fibo(n-2, memo);
+			memo[n]=fibo(n-1, memo)+fibo(n-2, memo);
 			
 		}
-		return memo[n-1];
+		return memo[n];
+		
+	}
+	
+	// top down approach
+	private static int fibotopdown(int n,int memo[]) {
+		
+		memo[0]=0;
+		memo[1]=1;
+		for(int i=2;i<=n;i++) {
+			memo[i]=memo[i-1]+memo[i-2];
+		}
+		return memo[n];
 		
 	}
 }
